914aa5878c09d3cf4b94b991fe3c20684ca65791
JangHyukJin <wkdgurwls00@naver.com>
2019-05-17 23:05:45 +0900

    Add upload_file function in view

--- Vcsite/mainapp/views.py
+++ Vcsite/mainapp/views.py
@@ -11,6 +11,31 @@ from django.views.decorators.csrf import csrf_exempt
 from mainapp.models import UploadFileModel
 from mainapp.models import Post
 
+@csrf_exempt
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('/mainapp/'))
+    else:
+        '''form = UploadFileForm()'''
+    print(form.files)
+    for filename, file in request.FILES.items():
+        name = request.FILES[filename].name
+    print(name)
+    post = UploadFileForm2(data=request.POST, files=request.FILES)
+    post = Post()
+    post.profile_pic = request.FILES.get('uploadfile')
+    post.pdf = request.FILES.get('uploadfile')
+    print(request.FILES.get('uploadfile'))
+    post.save()
+    '''
+    uploadfilemodel = UploadFileModel()
+    uploadfilemodel.title = request.POST.get('uploadfile', None)
+    print(request.POST.get('uploadfile'))
+    uploadfilemodel.save()'''
+    return render(request, 'mainapp/index.html')
 
 def index(request):
     template = loader.get_template('mainapp/index.html')
