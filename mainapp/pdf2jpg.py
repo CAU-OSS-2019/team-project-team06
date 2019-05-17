b843ba76edb86b22b4b3a07ccb481955e7d15d78
jisuAn <ajs7270@naver.com>
2019-05-18 00:51:51 +0900

    functionally merge

--- mainapp/pdf2jpg.py
+++ mainapp/pdf2jpg.py
@@ -4,6 +4,9 @@ import logging
 import os
 from pdf2image import convert_from_path
 
+import keywordfunction as kf
+from detect import detect_text 
+
 logging.basicConfig(
      level=logging.INFO,
      format='{%(pathname)s:%(lineno)d} %(levelname)s - %(message)s'
@@ -14,8 +17,26 @@ def list_pdfs(path='input'):
     pdfs = [i for i in files if i.split('.')[-1] == 'pdf']
     return pdfs
 
+
 def convert_resume_to_text(file_path, dpi=200):
     pages = convert_from_path(file_path, dpi)
+    full_text = ""
+    try:
+        logging.info('No. of pages: {}'.format(len(pages)))
+        page_len = len(pages)
+        for i, page in enumerate(pages):
+            page_text = detect_text(page) #OCR
+            full_text += page_text 
+
+            current_page = i + 1
+            logging.info('Processing page: {}'.format(current_page))
+
+    except Exception as e:
+        logging.warning('Error: {}'.format(e))
+
+    #키워드 추출
+    text_list =[full_text]
+    print(r.MakeKeyword())
 
 async def convert(path='input', dpi=200):
     try:
@@ -43,4 +64,4 @@ if __name__ == '__main__':
     ap = vars(parser.parse_args())
     resume_dir = ap['input']
     loop = asyncio.get_event_loop()
- 
+    loop.run_until_complete(main(resume_dir))
