61100b3b1691ad9db2225e5a7ec79e72dab17af5
jisuAn <ajs7270@naver.com>
2019-05-17 23:05:01 +0900

    add main function

--- website/pdf2jpg.py
+++ website/pdf2jpg.py
@@ -1,4 +1,20 @@
 import argparse
+import asyncio
+
+async def convert(path='input', dpi=200):
+    files = list_pdfs(path)
+
+    if len(files) > 0:
+        for file in files:
+            file_path = os.path.join(path, file)
+           
+            await asyncio.sleep(0.01)
+
+
+async def main(path, dpi=200):
+    await asyncio.wait([
+        convert(path=path, dpi=dpi)
+    ])
 
 if __name__ == '__main__':
 
