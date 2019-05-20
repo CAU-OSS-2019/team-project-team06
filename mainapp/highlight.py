ada3d02b4ed581630801f2e042476d033d661abf
JunSeokLee <shawn950303@gamil.com>
2019-05-20 15:33:46 +0900

    modify highlight.py devide into two cases for compare with keywords

--- mainapp/highlight.py
+++ mainapp/highlight.py
@@ -3,9 +3,14 @@ import string
 
 def highlight(files, texts_list, keyword_list):
     for i,pages in enumerate(files):
-         for j, page in enumerate(pages):
-         invert_img = page.load()
-         for k, word in enumerate(texts_list[i][j]):
-              if k == 0:
-                   pass
-                                                        
+        for j, page in enumerate(pages):
+        invert_img = page.load()
+        for k, word in enumerate(texts_list[i][j]):
+             if k == 0:
+                  pass
+             else:
+                 lower_word = word.description.lower().strip(string.punctuation)
+                 if k == 1:#first word of page
+
+                 else:
+                                                                
