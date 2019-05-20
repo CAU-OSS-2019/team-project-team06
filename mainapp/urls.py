9edc997748e34056f022f5e810ec5d0d64794338
JangHyukJin <wkdgurwls00@naver.com>
2019-05-20 17:10:54 +0900

    Add convert function call button(ajax)

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -10,6 +10,7 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('Upload/', views.upload_file, name='upload'),
+    path('Send/', views.upload_final, name='send'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
