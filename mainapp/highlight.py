3e010df9252484ebd00463cb563a0627c0cbe634
JunSeokLee <shawn950303@gamil.com>
2019-05-20 15:20:08 +0900

    modify highlight.py get pages from page, get page from pages, and get page pixel data

--- mainapp/highlight.py
+++ mainapp/highlight.py
@@ -2,4 +2,7 @@ from PIL import Image
 import string
 
 def highlight(files, texts_list, keyword_list):
-    
+    for i,pages in enumerate(files):
+         for j, page in enumerate(pages):
+         invert_img = page.load()
+
