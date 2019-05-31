473f6606cd1cad51d126b11e1e1cca3b2258738c
unknown <albert507@naver.com>
2019-05-31 19:46:43 +0900

    modify keywordfunction

--- mainapp/keywordfunction.py
+++ mainapp/keywordfunction.py
@@ -2,7 +2,6 @@ import math
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
-import gc
 from collections import Counter
 from nltk.tag import pos_tag
 
@@ -11,11 +10,6 @@ class keywordfunction:
     def __init__(self, StringBox):
         self._StringBox = StringBox
         nltk.download('averaged_perceptron_tagger')
-
-    def WordRank(self, x):
-        self._x = x
-
-
 
     def MakeKeyword(self):
         # corpus 전체 말뭉치
@@ -28,7 +22,9 @@ class keywordfunction:
             number += 1
 
         num_docs = len(corpus)
-
+        for i in corpus:
+            print(len(i))
+        print(num_docs)
         v = CountVectorizer(ngram_range=(1, 2), binary=True, stop_words='english', min_df=1)
         vf = v.fit_transform(corpus)
 
@@ -38,40 +34,24 @@ class keywordfunction:
         custom_weight1 = np.array([1 + math.log10(f) for f in freqs])
 
         keywords1 = []
-        interval = 100
-        cur_index = 0
 
-        while cur_index < len(corpus):
-            v = CountVectorizer(ngram_range=(1, 2), stop_words='english', vocabulary=terms, min_df=1)
-            vf = v.fit_transform(corpus[cur_index:cur_index + interval])
-            a = vf.toarray()
+        v = CountVectorizer(ngram_range=(1, 2), stop_words='english', vocabulary=terms, min_df=1)
+        vf = v.fit_transform(corpus)
+        a = vf.toarray()
 
-            #word 의 weight 계산
-            for data in a:
-                zero_weight = []
-                word_weight = np.copy((np.log10(data + 1) * idfs * custom_weight1))
-                for i, weight in enumerate(word_weight):
-                    if weight == 0:
-                        zero_weight.append(i)
+        # word 의 weight 계산
+        for data in a:
+            zero_weight = []
+            word_weight = np.copy((np.log10(data + 1) * idfs * custom_weight1))
+            for i, weight in enumerate(word_weight):
+                if weight == 0:
+                    zero_weight.append(i)
+            # weight이 '0'인 단어들 제거
+            new_word_weight = np.delete(word_weight, zero_weight)
 
-                #weight이 '0'인 단어들 제거
-                new_word_weight = np.delete(word_weight, zero_weight)
-
-                #for i, k in enumerate(sorted(new_word_weight)):
-                #    print(terms[i])
-                #    print(k)
-
-                #TOP20만 출력
-                x = word_weight.argsort()[-20:][::-1]
-                keywords1.append(x)
-
-
-            del v
-            del vf
-            del a
-            gc.collect()
-            cur_index += interval
-        # 메모리문제때문에 키워드 100간격씩 추출하고 메모리 해제
+            # TOP20만 출력
+            x = new_word_weight.argsort()[-20:][::-1]
+            keywords1.append(x)
 
         x1 = np.array(keywords1).flatten()
 
