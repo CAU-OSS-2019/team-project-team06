edf07f16c89f871503554db074ccba4ccf0c930f
unknown <albert507@naver.com>
2019-06-01 17:29:06 +0900

    modfiy keywordfunction.py for testing

--- mainapp/keywordfunction.py
+++ mainapp/keywordfunction.py
@@ -1,13 +1,14 @@
 import math
 from sklearn.feature_extraction.text import CountVectorizer
+import nltk
 import numpy as np
-import gc
 from collections import Counter
 from nltk.tag import pos_tag
 
 class keywordfunction:
     def __init__(self, StringBox):
         self._StringBox = StringBox
+        nltk.download('averaged_perceptron_tagger')
 
     def MakeKeyword(self):
         # corpus 전체 말뭉치
@@ -23,31 +24,23 @@ class keywordfunction:
 
         v = CountVectorizer(ngram_range=(1, 2), binary=True, stop_words='english', min_df=1)
         vf = v.fit_transform(corpus)
-        
+
         terms = v.get_feature_names()
         freqs = np.asarray(vf.sum(axis=0).ravel())[0]
         idfs = [math.log10(num_docs / f) for f in freqs]
         custom_weight1 = np.array([1 + math.log10(f) for f in freqs])
 
         keywords1 = []
-        interval = 100
-        cur_index = 0
 
-        while cur_index < len(corpus):
-            v = CountVectorizer(ngram_range=(1, 2), stop_words='english', vocabulary=terms, min_df=1)
-            vf = v.fit_transform(corpus[cur_index:cur_index + interval])
-            a = vf.toarray()
+        v = CountVectorizer(ngram_range=(1, 2), stop_words='english', vocabulary=terms, min_df=1)
+        vf = v.fit_transform(corpus[0:num_docs])
+        a = vf.toarray()
 
-            for data in a:
-                x = np.copy((np.log10(data + 1) * idfs * custom_weight1).argsort()[-20:][::-1])
-                keywords1.append(x)
-
-            del v
-            del vf
-            del a
-            gc.collect()
-            cur_index += interval
-        # 메모리문제때문에 키워드 100간격씩 추출하고 메모리 해제
+        for data in a:
+            word_weight = np.copy((np.log10(data + 1) * idfs * custom_weight1))
+            # TOP20만 출력
+            x = word_weight.argsort()[-20:][::-1]
+            keywords1.append(x)
 
         x1 = np.array(keywords1).flatten()
 
@@ -75,25 +68,23 @@ class keywordfunction:
                     else:
                         survived[text_num[i]].append(word)
 
-
         finalkeyword = []
         for s in text_num:
             output = [terms[x] for x in survived[s]]
             finalkeyword.append(output)
-        #print(finalkeyword)
+        # print(finalkeyword)
 
         tagged_list = []
         for i in finalkeyword:
             tagged_list.append(pos_tag(i))
-        #print(tagged_list)
-
+        # print(tagged_list)
 
         checkbox = []
         for i in tagged_list:
             wordbox2 = [t[0] for t in i if t[1] == "NN" or t[1] == "NNS"]
             checkbox.append(wordbox2)
 
-        #중복값 및 의미없는 단어
+        # 중복값 및 의미없는 단어
         finalbox = []
         for i in checkbox:
             resultbox = i
